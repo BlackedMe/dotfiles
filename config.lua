025e03c997d59c2a890120bb0c8b70abc640c50c
Black <44513102+BlackedMe@users.noreply.github.com>
2025-01-30 23:34:37 +0800

    Update config.lua

--- config.lua
+++ config.lua
@@ -92,9 +92,9 @@ lvim.colorscheme = "tokyodark"
 -- lvim.colorscheme = "neofusion"
 -- mapping
 
+
 -- compile file
 lvim.keys.normal_mode['<C-A-b>'] = ':!g++ -g % -o %:r <cr>'
-
 -- compile and run
 lvim.keys.normal_mode['<C-A-n>'] = ':cd %:h | :!g++ -g % -o %:r <cr>| :!./%:r <cr>'
 
@@ -113,7 +113,7 @@ vim.keymap.set('n', '<F11>', function() require('dap').step_into() end)
 vim.keymap.set('n', '<F12>', function() require('dap').step_out() end)
 vim.keymap.set('n', '<A-b>', function() require('dap').toggle_breakpoint() end)
 vim.keymap.set('n', '<A-c>', function() require('dap').clear_breakpoints() end)
-vim.keymap.set('n', '<C-A-t>', function() require('dapui').toggle() end)
+vim.keymap.set('n', '<C-A-d>', function() require('dapui').toggle() end)
 vim.keymap.set('n', '<A-BS>', function() require('dap').disconnect() end)
 
 -- toggle floating (function)signature
